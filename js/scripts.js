11b64976ff48d0215ffb49305867fe69f1066cc7
Milada <greatchocopie@yandex.com>
2020-07-11 18:26:52 +0800

    сделала открытие и закрытие карты

--- js/scripts.js
+++ js/scripts.js
@@ -8,54 +8,29 @@ var form = popupLetter.querySelector(".form-flex");
 var userName = popupLetter.querySelector(".name");
 var eMail = popupLetter.querySelector(".e-mail");
 
-let open = function (evt) {
+function open(evt) {
   evt.preventDefault();
   popupMap.classList.add("modal-show");
 
 };
-let close = function (evt) {
+
+function close(evt) {
   evt.preventDefault();
-
   popupMap.classList.remove("modal-show");
-
+  document.removeEventListener("click", close);
 }
 map.addEventListener("click", open);
 document.addEventListener("click", function (evt) {
   var target = evt.target;
-  if (popupMap.closest(".modal-show") && target !== popupMap) {
+  if (popupMap.closest(".modal-show") && (target !== popupMap) || (target === closeMap)) {
     document.addEventListener("click", close);
-  } else {
-    document.removeEventListener("click", close);
   }
 });
-
-closeMap.addEventListener("click", close);
-
-// if (open == true) {
-//   window.addEventListener("click", function (evt) {
-//     evt.preventDefault();
-//     var target = evt.target;
-//     if (popupMap.classList.contains("modal-show") && target !== popupMap) {
-//       popupMap.classList.remove("modal-show");
-//       console.log("lssl;");
-//     }
-//   });
-// };
-
-
-
-
-
-
-
-
-
-
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (popupMap.classList.contains("modal-show")) {
-      popupMap.classList.remove("modal-show");
+      window.addEventListener("keydown", close);
     }
   }
 });
