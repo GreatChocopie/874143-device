217e11cd2f8a8de6d1e26d0ea149f0eb9dca4f71
Milada <greatchocopie@yandex.com>
2020-07-14 15:03:18 +0800

    готовы попапы

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,21 @@ function openMap(evt) {
   evt.preventDefault();
   evt.stopPropagation();
   popupMap.classList.add("modal-show");
-  document.addEventListener("click", close);
+  document.addEventListener("click", mapClose);
+};
+
+function openLetter(evt) {
+  if (popupLetter.classList.contains("modal-show")) {
+    console.log("already opened letter");
+    evt.preventDefault();
+    return false;
+  }
+  console.log("opening letter");
+  evt.preventDefault();
+  evt.stopPropagation();
+  popupLetter.classList.add("modal-show");
+  userName.focus();
+  document.addEventListener("click", letterClose);
 };
 
 // Ищем среди родителей elem присутствует ли neelde
@@ -33,7 +47,7 @@ function elemHasParent(elem, needle) {
 
 
 
-function close(evt) {
+function mapClose(evt) {
   console.log("close start");
   console.log(evt.target);
   var target = evt.target;
@@ -43,38 +57,49 @@ function close(evt) {
     console.log("closing map");
     evt.preventDefault();
     popupMap.classList.remove("modal-show");
-    document.removeEventListener("click", close);
+    document.removeEventListener("click", mapClose);
     console.log("listener removed");
   } else {
     console.log("close - wrong elem, do nothing");
   }
 }
+
+function letterClose(evt) {
+  console.log("close start");
+  console.log(evt.target);
+  var target = evt.target;
+  // если это кнопка закрытия или элемент среди родителей которого нет popupMap и
+  // то закрываем
+  if (!elemHasParent(target, popupLetter) || target == closeLetter) {
+    console.log("closing letter");
+    evt.preventDefault();
+    popupLetter.classList.remove("modal-show");
+    popupLetter.classList.remove("modal-error");
+    document.removeEventListener("click", letterClose);
+    console.log("listener removed");
+  } else {
+    console.log("close - wrong elem, do nothing");
+  }
+}
+
 map.addEventListener("click", openMap);
-console.log("js is painful");
+
+contacts.addEventListener("click", openLetter);
+
 
 
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (popupMap.classList.contains("modal-show")) {
-      window.addEventListener("keydown", close);
+      window.addEventListener("keydown", mapClose);
     }
   }
 });
 
 
 
-contacts.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupLetter.classList.add("modal-show");
-  userName.focus();
-});
-closeLetter.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupLetter.classList.remove("modal-show");
-  popupLetter.classList.remove("modal-error");
 
-});
 
 
 
